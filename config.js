637c9285c2205dba30dcc000233a99e5a95d7c35
Leo <1074699327@qq.com>
2018-05-14 16:24:08 +0800

    fixBUG

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const isDev = process.env.NODE_ENV === 'development'
 
 module.exports = {
     port: 3000,
-    mongodb: isDev ? 'mongodb://127.0.0.1:27017/clubs_db' : 'mongodb://clubs_runner：clubs1226@127.0.0.1:39999/clubs',
+    mongodb: isDev ? 'mongodb://127.0.0.1:27017/clubs_db' : 'mongodb://clubs_runner:clubs1226@127.0.0.1:39999/clubs',
 
     staticDir: 'static',
     imageSaveDir: '/image'
